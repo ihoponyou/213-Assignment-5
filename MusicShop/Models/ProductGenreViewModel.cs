81611b41eb29fdb3abb540623966ea4da1b62e14
Christopher Wagner <CJWagner2002@gmail.com>
2023-12-07 01:08:44 -0600

    made chagnes to design and search

---
+++ MusicShop/Models/ProductGenreViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MusicShop.Models
+{
+    public class ProductGenreViewModel
+    {
+        public List<Product> Products { get; set; }
+        public SelectList? Genres { get; set; }
+
+        public SelectList? Performers { get; set; }
+        public string? ProductGenre {  get; set; }
+
+        public string? ProductPerformer { get; set; }
+        public string? SearchString {  get; set; }
+    }
+}
