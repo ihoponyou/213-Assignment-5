42dd7168e90fb712adc92a7c27e374aed17f93b1
Christopher Wagner <CJWagner2002@gmail.com>
2023-12-07 01:09:02 -0600

    made changes to design and search

---
+++ MusicShop/Models/ProductPerformerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MusicShop.Models
+{
+    public class ProductPerformerViewModel
+    {
+        public List<Product> Products { get; set; }
+        public SelectList? Performer { get; set; }
+        public string? ProductPerformer { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
